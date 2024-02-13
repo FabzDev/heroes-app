e3d4813d5cd02d6ada01030600849615f5f206a3
Fabio <fabioescobarardila@hotmail.com>
2024-02-13 14:31:09 -0500

    Revert "v2"
    This reverts commit 2816dd637b18be4243b8bc2e05e66c69e2723cc1.

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,10 @@ import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { CardComponent } from './components/card/card.component';
+<<<<<<< HEAD
+=======
+import { CardImgPipe } from './pipes/card-img.pipe';
+>>>>>>> parent of 2816dd6 (v2)
 
 
 
@@ -20,7 +24,12 @@ import { CardComponent } from './components/card/card.component';
     NewPageComponent,
     SearchPageComponent,
     HeroPageComponent,
+<<<<<<< HEAD
     CardComponent
+=======
+    CardComponent,
+    CardImgPipe
+>>>>>>> parent of 2816dd6 (v2)
   ],
   imports: [
     CommonModule,
