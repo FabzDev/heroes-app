ce554502dcb3aa924750e4379496278c040020d5
Fabio <fabioescobarardila@hotmail.com>
2024-02-12 21:00:39 -0500

    Heroes: Implementación del SideNav

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LayoutPageComponent {
-
+public sideNavItems = [
+  {
+    name: 'Listado',
+    icon: 'list',
+    rLink: 'list'
+  },
+  {
+    name: 'Añadir',
+    icon: 'add',
+    rLink: 'new-hero'
+  },
+  {
+    name: 'Buscar',
+    icon: 'search',
+    rLink: 'search'
+  }
+]
 }
