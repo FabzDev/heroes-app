bbd1aa4cf2bdcedd6441343a9ed66d317079a3fd
Fabio <fabioescobarardila@hotmail.com>
2024-02-15 02:54:25 -0500

    New page: Formulario creado

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class NewPageComponent {
-
+  public creadores = [
+    {
+      name: 'DC Comics',
+      display: 'DC - Comics',
+    },
+    {
+      name: 'Marvel Comics',
+      display: 'Marvel - Comics',
+    },
+  ];
 }
