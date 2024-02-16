2d19a1cc3f7c0806b7ace26af92e080211a86cfd
Fabio <fabioescobarardila@hotmail.com>
2024-02-16 02:44:11 -0500

    Obteniendo el current hero por medio del heroForm.value

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
-import { Publisher } from '../../interfaces/heroes.interface';
+import { Hero, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -20,12 +20,22 @@ export class NewPageComponent {
     },
   ];
 
-  public searchForm = new FormGroup({
+  public heroForm = new FormGroup({
     id: new FormControl<string>(''),
     superhero: new FormControl<string>('', {nonNullable: true}),
     publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl<string>(''),
     first_appearance: new FormControl<string>(''),
     characters: new FormControl<string>(''),
     alter_img: new FormControl<string>(''),
   })
+
+  get currentHero(): Hero {
+    return this.heroForm.value as Hero;
+  }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
+  }
 }
