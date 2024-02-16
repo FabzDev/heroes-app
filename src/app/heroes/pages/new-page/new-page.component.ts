47a5bfad264a56e56fe7da4b9804ab3966bfb399
Fabio <fabioescobarardila@hotmail.com>
2024-02-16 00:13:21 -0500

    new-page-component.ts creada logica de formulario reactivo

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -16,4 +19,13 @@ export class NewPageComponent {
       display: 'Marvel - Comics',
     },
   ];
+
+  public searchForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', {nonNullable: true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    first_appearance: new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alter_img: new FormControl<string>(''),
+  })
 }
